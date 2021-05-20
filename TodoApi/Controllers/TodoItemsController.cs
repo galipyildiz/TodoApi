d516ed1457fa404b0618875f1c682216df8923f2
Galip Yıldız <galipyildiz9606@outlook.com>
2021-05-21 01:46:13 +0300

    Add project files.

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _todoContext;
+
+        public TodoItemsController(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _todoContext.TodoItems.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _todoContext.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _todoContext.Entry(todoItem).State = EntityState.Modified;
+            try
+            {
+                await _todoContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _todoContext.TodoItems.Add(todoItem);
+            await _todoContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _todoContext.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            _todoContext.TodoItems.Remove(todoItem);
+            await _todoContext.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool TodoItemExists(long id)
+        {
+            return _todoContext.TodoItems.Any(x => x.Id == id);
+        }
+    }
+}
